720ff8554c410d7994aa90ff79726d9b1808200a
Ghost <1129.gh0st@gmail.com>
2022-07-13 11:51:35 +0530

    Update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../../Components/Hero/Hero'
 import Navbar from '../../Components/Navbar'
+
+import Products from '../../Components/Products'
 import './Home.sass'
 
 const Home = () => {
+
+  let [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetchData()
+    }, [])
+  
+    async function fetchData() {
+      const items = await fetch('https://fakestoreapi.com/products').then(
+          (response) => response.json()
+      )
+      setProducts(items);
+      }
+
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Products products ={products}/>
+    
     </>
     
   )
